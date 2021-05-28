5f4feb0546b0d96e03b7b85b35335aa2c5463af8
(none) <john@iamjohnhenry.com>
2021-05-28 16:30:46 -0700

    remove intentional error

--- src/util/fetch-data.js
+++ src/util/fetch-data.js
@@ -3,7 +3,6 @@ const fetchData = async (
   url = "https://60a45748fbd48100179dbe04.mockapi.io/frontend/items"
 ) => {
   try {
-    throw new Error("remove after testing");
     return (await window.fetch(url)).json();
   } catch ({ message }) {
     console.warn("Loading backup due to error: ", message);
